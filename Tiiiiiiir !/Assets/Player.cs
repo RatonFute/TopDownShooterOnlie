7bfd8ff81dd7f53ff2f8978fd7bb24ce6aaa30eb
Hugo Caroumbin | "RatonFuté <hcaroumbin@gaming.tech>
2024-05-07 19:13:59 +0200

    player prefab

---
+++ Tiiiiiiir !/Assets/Player.cs
@@ -0,0 +1,15 @@
+using Mirror;
+using UnityEngine;
+
+public class Player : NetworkBehaviour
+{
+    [SerializeField] private Vector3 _movement = new Vector3 (0, 0, 0);
+
+    [Client]
+    private void Update()
+    {
+        if (!authority) { }
+        if (!Input.GetKeyDown(KeyCode.A)) return;
+        transform.Translate(_movement);
+    }
+}
